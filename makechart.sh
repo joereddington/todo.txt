5719d96db3967ddc0150b70c0a81c805f14ae1e3
Joseph Reddington <josephreddington@Josephs-Mini.fritz.box>
2018-09-07 19:50:53 +0100

    Update before chart

--- makechart.sh
+++ makechart.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 rm log.csv
 cp regression/takesnapshot.sh read.sh
 touch log.csv
-git rebase  --exec read.sh 0fd691acb0b44d9621bbdf75779790ca14d86ef0
+git rebase  --exec read.sh 0fd691acb0b44d9621bbdf75779790ca14d86ef0 --preserve-merges
 mv log.csv charts/logsize.csv
 cd charts
 python plotPri.py  -f logsize.csv   -c -t 7 -o fear.png
