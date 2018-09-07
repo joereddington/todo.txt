c9afb3a3a81650df92e4eff7210bfb8a9a341821
Joseph Reddington <josephreddington@Josephs-Mini.fritz.box>
2018-09-07 19:54:57 +0100

    Update before chart

--- makechart.sh
+++ makechart.sh
@@ -5,7 +5,7 @@ rm log.csv
 cp regression/takesnapshot.sh read.sh
 chmod a+x read.sh
 touch log.csv
-git rebase  --exec read.sh 0fd691acb0b44d9621bbdf75779790ca14d86ef0 --preserve-merges
+git rebase  --exec ./read.sh --root --preserve-merges
 mv log.csv charts/logsize.csv
 cd charts
 python plotPri.py  -f logsize.csv   -c -t 7 -o fear.png
