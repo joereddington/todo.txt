e5b6cc1ed48481b970c3b062889b4d95534fe789
Joe Reddington <joe@joereddington.com>
2018-09-07 09:53:03 +0100

    pre merge

--- makechart.sh
+++ makechart.sh
@@ -8,6 +8,7 @@ git rebase  --exec read.sh 0fd691acb0b44d9621bbdf75779790ca14d86ef0
 mv log.csv charts/logsize.csv
 cd charts
 python plotPri.py  -f logsize.csv   -c -t 7 -o fear.png
+cd ..
 rm read.sh #cleaning up afteroutselves. 
 git commit -a -m "Chart included" 
 git push origin master
