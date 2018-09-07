4028e9ae89b3c2970dd8a3712fe8d4529f7c1e5b
Joe Reddington <joe@joereddington.com>
2018-09-07 09:51:01 +0100

    brining in charts

--- makechart.sh
+++ makechart.sh
@@ -4,4 +4,7 @@ rm log.csv
 cp regression/takesnapshot.sh read.sh
 touch log.csv
 git rebase  --exec read.sh 0fd691acb0b44d9621bbdf75779790ca14d86ef0
+mv log.csv charts/logsize.csv
+cd charts
+python plotPri.py  -f logsize.csv   -c -t 7 -o fear.png
 
