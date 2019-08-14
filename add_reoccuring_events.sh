d5b5e7356f5d4d1d03a4cb4e0e3bc298db58de36
Joe Reddington <joe@joereddington.com>
2019-08-14 15:51:51 +0100

    igor sorting now working

--- add_reoccuring_events.sh
+++ add_reoccuring_events.sh
@@ -1,7 +1,7 @@
 sort todo.txt > sorted.all
-sort todo.txt > sorted.eqt
+sort eqt.todo.txt > sorted.eqt
 igor/igor -d > igor.txt 
 grep "EQT" igor.txt >> sorted.eqt 
 grep -v "EQT" igor.txt >> sorted.all
 mv sorted.all todo.txt
-mv sorted.eqt todo.txt
+mv sorted.eqt eqt.todo.txt
