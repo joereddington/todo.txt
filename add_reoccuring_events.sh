255682cbbd9aa48512b50ff97f90dbf35d29d28c
Joe Reddington <joe@joereddington.com>
2019-08-14 15:45:48 +0100

    spliting igor

--- add_reoccuring_events.sh
+++ add_reoccuring_events.sh
@@ -1,3 +1,7 @@
-sort todo.txt > sorted.txt 
-igor/igor -d >> sorted.txt
-mv sorted.txt todo.txt
+sort todo.txt > sorted.all
+sort todo.txt > sorted.eqt
+igor/igor -d > igor.txt 
+grep "EQT" igor.txt >> sorted.eqt 
+grep -v "EQT" igor.txt >> sorted.all
+mv sorted.all todo.txt
+mv sorted.eqt todo.txt
