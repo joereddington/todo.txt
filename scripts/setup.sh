833835a4af38cd2e3e69e1ec119b0d65b457f1e3
Joseph Reddington <joe@joereddington.com>
2019-11-04 08:58:05 +0000

    I've changed the config so that we automatically check out the most recent version of the submodules. They still checkout detached though, which is annoying

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,5 +3,5 @@ cd "$(dirname "$0")"
 cd .. 
 git config core.commentChar ";"
 #allows me to use markdown in the comments 
-git submodule update --init 
+git submodule update --init --remote  
 #Checks out all the submodules 
