e9a55e9114d2792b9b3e763cdfd8e6d401e0e522
Joseph Reddington <joe@joereddington.com>
2020-02-24 06:10:29 +0000

    ## 23/02/20 07:52 to 08:35, Using the SE response to move forward with vision +EQT Putting the new vision into the toolchain
    ## 23/02/20 08:35 to 09:05, Working on https://github.com/joereddington/Projects-Public/issues/1 +PersonalProject
    I reviewed the issue and closed it after some thought. It doesn't help
    to blog it.

--- scripts/add_reoccuring_events.sh
+++ scripts/add_reoccuring_events.sh
@@ -4,7 +4,10 @@ cd ..
 sort todo.txt > sorted.all
 sort eqt.todo.txt > sorted.eqt
 submodules/igor/igor -d > igor.txt 
+python3 submodules/vision/vision.py -d > vision.txt 
 grep "EQT" igor.txt >> sorted.eqt 
 grep -v "EQT" igor.txt >> sorted.all
+grep "EQT" vision.txt >> sorted.eqt 
+grep -v "EQT" vision.txt >> sorted.all
 mv sorted.all todo.txt
 mv sorted.eqt eqt.todo.txt
