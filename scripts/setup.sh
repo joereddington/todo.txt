b0d6327492a64bfe4a1e32ff3f31d6c62e013138
Joseph Reddington <joe@joereddington.com>
2022-04-12 10:03:26 +0100

    Clear out for github building

--- scripts/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-cd .. 
-git config core.commentChar ";"
-#allows me to use markdown in the comments 
-git submodule update --init --remote  
-#Checks out all the submodules 
